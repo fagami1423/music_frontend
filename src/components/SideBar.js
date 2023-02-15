32e41c28d277b0c1b48c61c978e161c6885c9696
Raj Kumar Phagami <magar.fagami@gmail.com>
2023-02-14 23:00:17 -0500

    fixed the circle div to the center

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,21 +4,23 @@ import { Link } from "react-router-dom";
 
 function Sidebar(props){
     return (
-        <div className="justify-content-center">
+        <div className="justify-content-center bg-dark">
             {/* Navigation links go here */}
             <div className="col">
                 <div className="row">
                     <h1>{props.title}</h1>
                 </div>
-                <div className="row justify-content-center custom-circle rounded-circle">
+                <div className="row justify-content-center">
+                    <div className="custom-circle rounded-circle">
                     
+                    </div>
                 </div>
                 <div className="row">
                 
-                    <ul className="list-group bg-secondary justify-content-center">
-                        <li class="list-group-item border-0 bg-secondary active"><Link className="text-light text-decoration-none" to="/">Homepage</Link></li>
-                        <li class="list-group-item border-0 bg-secondary"><Link className="text-light text-decoration-none" to="/musiclist">Music List</Link></li>
-                        <li class="list-group-item border-0 bg-secondary"><Link className="text-light text-decoration-none" to="/contact">Contact</Link></li>
+                    <ul className="list-group bg-dark justify-content-center">
+                        <li class="list-group-item border-0 bg-dark active"><Link className="text-light text-decoration-none" to="/">Homepage</Link></li>
+                        <li class="list-group-item border-0 bg-dark"><Link className="text-light text-decoration-none" to="/musiclist">Music List</Link></li>
+                        <li class="list-group-item border-0 bg-dark"><Link className="text-light text-decoration-none" to="/contact">Contact</Link></li>
                     </ul>
                 </div>
                 
