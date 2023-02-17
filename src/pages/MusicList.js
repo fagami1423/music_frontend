9df7f44b43562d6303ad4e8855a10bc08cee5ca9
Raj Kumar Phagami <magar.fagami@gmail.com>
2023-02-16 22:30:00 -0500

    making it ready for the music player

--- src/pages/MusicList.js
+++ src/pages/MusicList.js
@@ -3,6 +3,8 @@ function MusicList(){
         <>
             <div className="row justify-content-center">
                 <img className="music-background" src={process.env.PUBLIC_URL+'/main.jpg'} alt="Main"></img>
+
+                <h1>General music player will be added here</h1>
             </div>
         </>
     )
