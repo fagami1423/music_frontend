a23a21d0d651725f5c2e6fbfdb6b726f4372f997
Raj Kumar Phagami <magar.fagami@gmail.com>
2023-03-03 20:46:31 -0500

    changed the chatbot js into hooks

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -1,80 +1,82 @@
 import React from 'react';
+import { useState } from 'react';
 import '../css/Chatbot.css';
 
-class Chatbot extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false, // boolean to track whether the chatbot is open or closed
-      messages: [], // array to store chat messages
-      inputValue: '', // input value for user messages
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleToggle = this.handleToggle.bind(this);
-  }
+// class Chatbot extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isOpen: false, // boolean to track whether the chatbot is open or closed
+//       messages: [], // array to store chat messages
+//       inputValue: '', // input value for user messages
+//     };
+//     this.handleInputChange = this.handleInputChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.handleToggle = this.handleToggle.bind(this);
+//   }
 
-  // function to handle input change
-  handleInputChange(event) {
-    this.setState({inputValue: event.target.value});
-  }
+//   // function to handle input change
 
+const Chatbot=(props)=>{
+  const [isOpen, setIsOpen] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState('');
   // function to handle message submission
-  handleSubmit(event) {
+  const handleSubmit = (event) =>{
     event.preventDefault();
+    // add new message to messages array
     const newMessage = {
-      text: this.state.inputValue,
+      text: inputValue,
       isUser: true // mark user messages as true
     };
-    console.log()
-    this.setState({
-      messages: [...this.state.messages, newMessage],
-      inputValue: ''
-    });
+
+    messages.push(newMessage);
+    setInputValue(event.target.value);
     // call function to handle bot response
-    this.handleBotResponse();
+    handleBotResponse();
+  }
+  const handleInputChange = (event) =>{ 
+    setInputValue(event.target.value);
   }
 
   // function to handle bot response
-  handleBotResponse() {
+  const handleBotResponse=() =>{
     // code to generate bot response goes here
     // add new message to messages array
     const botResponse = {
       text: "Hi! How can I assist you?",
       isUser: false // mark bot messages as false
     };
-    this.setState({
-      messages: [...this.state.messages, botResponse]
-    });
+    messages.push(botResponse);
+    setMessages(messages);
   }
 
   // function to handle chatbot toggle button
-  handleToggle() {
+  const handleToggle = () =>{
     console.log("im here");
-    this.setState({isOpen: !this.state.isOpen});
+    setIsOpen(!isOpen);
   }
 
-  render() {
     return (
       <div>
-        <button className="btn btn-secondary Chatbot-toggle" onClick={this.handleToggle}>
-          {this.state.isOpen ? "Close Chatbot" : "Open Chatbot"}
+        <button className="btn btn-secondary Chatbot-toggle" onClick={handleToggle}>
+          {isOpen ? "Close Chatbot" : "Open Chatbot"}
         </button>
-        <div className={`Chatbot ${this.state.isOpen ? "open" : "closed"}`}>
-          {this.state.isOpen &&
+        <div className={`Chatbot ${isOpen ? "open" : "closed"}`}>
+          {isOpen &&
             <>
               <div className="Chatbot-messages">
-                {this.state.messages.map((message, index) => (
+                {messages.map((message, index) => (
                   <div key={index} className={message.isUser ? "User-message" : "Bot-message"}>
                     {message.text}
                   </div>
                 ))}
               </div>
-              <form onSubmit={this.handleSubmit} className="Chatbot-form">
+              <form onSubmit={handleSubmit} className="Chatbot-form">
                 <input
                   type="text"
-                  value={this.state.inputValue}
-                  onChange={this.handleInputChange}
+                  value={inputValue}
+                  onChange={handleInputChange}
                   placeholder="Type a message..."
                 />
                 <button type="submit">Send</button>
@@ -84,9 +86,7 @@ class Chatbot extends React.Component {
         </div>
       </div>
     );
-  }
-  
-  
+
 }
 
 export default Chatbot;
