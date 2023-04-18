10eb24c4ebd696788133e06dafeae51ed51cfdc2
Raj Kumar Phagami <magar.fagami@gmail.com>
2023-04-18 09:58:32 -0400

    base url changed

--- src/Config.js
+++ src/Config.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-const baseUrl = "http://localhost:8000";
-const apiUrl = "http://localhost:8000/api/v1";
+// const baseUrl = "http://localhost:8000";
+// const apiUrl = "http://localhost:8000/api/v1";
+const baseUrl = "http://34.135.155.173:8000"
+const apiUrl = "http://34.135.155.173:8000/api/v1"
 const api = axios.create({
   baseURL: apiUrl
 });
